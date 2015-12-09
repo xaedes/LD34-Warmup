3ed915e6d8f3b4838f08c1327708c09dec7a6c90
xaedes <xaedes@gmail.com>
2015-12-09 15:37:02 +0100

    added description of gameplay

--- src/js/states/gameplay.js
+++ src/js/states/gameplay.js
@@ -5,7 +5,11 @@ define([], function() {
 
     GameplayState.prototype = {
         create: function() {
-                        
+            // add tank
+            // control tank with wasd and with arrows
+            // add mouse controlled bullseye
+            // click causes tank to shoot
+            // bullet flies in parable, height causes upscaling, simply use tween?
         }
     };
 
