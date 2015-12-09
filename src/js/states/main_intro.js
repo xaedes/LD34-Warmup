c3e72d7bc32daa5f6d40d88db3ad37f0b4a6ff82
xaedes <xaedes@gmail.com>
2015-12-09 15:01:53 +0100

    added intro screen with simple "Tanks" title

--- src/js/states/main_intro.js
+++ src/js/states/main_intro.js
@@ -5,12 +5,22 @@ define(['phaser'], function(Phaser) {
 
     MainIntroState.prototype = {
         create: function() {
-            this.tweenFadeState();
+            var title_text = this.game.add.text(this.game.world.centerX,this.game.world.centerY, 
+                "Tanks", 
+                {font: "60px shmupfont", fill: "#ffffff", stroke: '#000000', strokeThickness: 3});
+            title_text.fixedToCamer = false;
+            title_text.anchor.setTo(0.5, 0.5);
+            title_text.wordWrap = true;
+            title_text.wordWrapWidth = (0.95 * this.game.world.width);
+            title_text.alpha = 0
+
+            // this.title_text = this.add.bitmapText(8, 364, 'shmupfont', "Tanks", 24);
+            this.tweenFadeState(title_text);
         },
         
-        tweenFadeState: function() {
-            this.game.add.tween({})
-                .to({alpha: 1}, 2000)
+        tweenFadeState: function(title_text) {
+            this.game.add.tween(title_text)
+                .to({alpha: 1}, 2000,"Cubic.easeOut",true)
                 .onComplete.add(function() {
                     this.game.state.start('main-menu');
                 }, this);
